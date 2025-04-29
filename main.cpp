d1e4c60863769d8c3ec5f3dac41eb862c959b19f
Facatura <gallardojuanbautista010@gmail.com>
2025-04-29 10:20:47 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,4 +9,6 @@ int main(){
     int presupuesto = 50000;
     int precio_soldados_m = 12500; // Precio por mil solados
     int precio_comida_m = 5000;    // Precio para alimentar mil soldados kokokokoko
+    int soyUnCrack = 1;
+    int soyElMomazo = 3; // thiago so juan
 }
