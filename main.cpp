eed11fe94087ccff18e2d84559b829c7f8759f68
InnoDev69 <yamirnu14@gmail.com>
2025-04-29 09:37:44 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    // Inicializacion de variables
+    int presupuesto = 50000;
+    int precio_soldados_m = 12500; // Precio por mil solados
+    int precio_comida_m = 5000;    // Precio para alimentar mil soldados
+    
+}
