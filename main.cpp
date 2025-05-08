23482627f74874280c43149b310e00746fd6a2b0
Dev_ <89705058+InnoDev69@users.noreply.github.com>
2025-05-07 21:32:44 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 
 using namespace std;
 
-int main() {
-    srand(time(NULL));
+int main()
+{
     /// (1) *************************************    INICIO DECLARACIÓN DE VARIABLES Y CONSTANTES  *************************************///
     /// JUGABILIDAD
     const int presupuesto_inicial = 50000;
@@ -27,18 +25,12 @@ int main() {
 
     // articulo: objeto que se compra en la tienda, el valor refiere a la cantidad.
     const int articulo_soldados        = 1000;
-    const int articulo_comida          = 1000;
+    const int articulo_comida          = 500;
     const float articulo_mejora_pasiva = 0.1;
 
     /// BATALLA
     const int duracion_guerra = 5;
-    const int rondas_por_batalla = 10;
-    const int max_derrotas = 4;
-    const float incremento_prob_x_soldados = 0.15f; // 15% por cada 1000 soldados
-    const int soldados_x_prob = 1000;
     int batalla_actual = 0;
-    int derrotas_totales = 0;
-    int ronda_actual = 0;
 
     /// ESTADISTICAS
     const int batalla_cantidad_victorias = 0;
@@ -77,12 +69,12 @@ int main() {
         cout << "            Juego de Tronos            "               << endl;
         cout << "---------------------------------------"               << endl;
         cout << "~TARGARYEN "                                           << endl;
-        cout << "|Batallas realizadas : " << batalla_actual                  << endl;
-        cout << "|Presupuesto         : " << presupuesto_inicial             << endl;
-        cout << "|Oro                 : " << oro                             << endl;
-        cout << "|Comida              : " << comida                          << endl;
-        cout << "|Soldados            : " << soldados                        << endl;
-        cout << "|Pasiva              : " << pasiva_probabilidad             << endl;
+        cout << "|batallas realizadas : " << batalla_actual                  << endl;
+        cout << "|presupuesto         : " << presupuesto_inicial             << endl;
+        cout << "|oro                 : " << oro                             << endl;
+        cout << "|comida              : " << comida                          << endl;
+        cout << "|soldados            : " << soldados                        << endl;
+        cout << "|pasiva              : " << pasiva_probabilidad             << endl;
         cout << "---------------------------------------"               << endl;
         cout << idx_opcion_menu_batalla << ". IR A LA BATALLA"          << endl;
         cout << idx_opcion_menu_tienda  << ". TIENDA"                   << endl;
@@ -145,72 +137,7 @@ int main() {
                 cout << endl;
             }
 
-            cout << "Batalla numero: " << batalla_actual;
-
-            // Simulacion de batalla
-            int soldados_combatientes = min(soldados, comida); // Solo pueden pelear los que pueden comer
-            if(soldados_combatientes <= 0) {
-                cout << "\nNo tenes suficientes soldados o comida para combatir!" << endl;
-                derrotas_totales++;
-            } else {
-                cout << "\nSoldados que entraran en combate: " << soldados_combatientes << endl;
-                
-                // Calcular probabilidad base de victoria
-                float prob_victoria = pasiva_probabilidad; // Probabilidad base
-                prob_victoria += (soldados_combatientes / soldados_x_prob) * incremento_prob_x_soldados;
-                
-                int victorias_ronda = 0;
-                for(ronda_actual = 1; ronda_actual <= rondas_por_batalla; ronda_actual++) {
-                    cout << "\nRonda " << ronda_actual << "..." << endl;
-                    
-                    // Aumentar dificultad cada 2 rondas
-                    /*Suma 1 al resultado para que la dificultad base sea 1.0
-                    Y despues multiplica el resultado anterior por 0.1, por lo que la dificultad 
-                    aumenta en incrementos de 0.1
-                    */
-                    float dificultad = 1.0f + (float)(ronda_actual / 2) * 0.1f; // Aumenta 10% cada 2 rondas
-                    float prob_final = prob_victoria / dificultad;
-                    
-                    // Determinar victoria de la ronda
-                    if((float)rand()/RAND_MAX < prob_final) {
-                        cout << "Victoria en la ronda!" << endl;
-                        victorias_ronda++;
-                    } else {
-                        cout << "Derrota en la ronda..." << endl;
-                    }
-                    
-                    // Calcular perdidas
-                    int porcentaje_perdidas = 5 * ronda_actual;
-                    int perdidas = (soldados_combatientes * porcentaje_perdidas) / 100;
-                    soldados_combatientes -= perdidas;
-                    soldados -= perdidas;
-                    
-                    cout << "Perdiste " << perdidas << " soldados" << endl;
-                    cout << "Soldados restantes: " << soldados << endl;
-                }
-                
-                // Determinar resultado final de la batalla
-                if(victorias_ronda > rondas_por_batalla/2) {
-                    cout << "\n¡Has ganado la batalla!" << endl;
-                    int oro_ganado = 10000 + batalla_actual * 5000;
-                    oro += oro_ganado;
-                    cout << "Oro ganado: " << oro_ganado << endl;
-                } else {
-                    cout << "\nHas perdido la batalla..." << endl;
-                    derrotas_totales++;
-                }
-                
-                comida = max(0, comida - soldados_combatientes); // Consumir comida usada
-            }
-
-            // Verificar condicion de derrota
-            if(derrotas_totales >= max_derrotas) {
-                cout << "\n¡Has perdido el juego! Los White Walkers han conquistado Westeros." << endl;
-                system("pause");
-                return 0;
-            }
-
-            /// LITERALMENTE ACA TERMINA TODO LO DEL COMBATE
+            cout << "Batalla NRO " << batalla_actual;
 
             cout << endl;
 
@@ -222,8 +149,6 @@ int main() {
         case idx_opcion_menu_tienda:
         {
             /// (4) -----------------------------------    MENÚ DE LA TIENDA  -----------------------------------///
-            cin_opcion_tienda = 0; // Reiniciar la opcion de la tienda al inicio del bucle por el maldito bug del diablo
-            
             while(true)
             {
                 if (cin_opcion_tienda == idx_opcion_tienda_volver)
@@ -329,17 +254,6 @@ int main() {
         case idx_opcion_menu_volver:
         {
             return 0;                   //<---- SALIR DEL JUEGO
-        }
-        default:
-        {
-            cout << "Opcion no valida!" << endl;
-
-            cout << endl;
-
-            cout << "Presiona cualquier tecla para volver al menu.";
-            system("pause");
-
-            break;
         }
         }
         /// (3) ***********************************  FIN ESTRUCTURA DE SELECCIÓN DEL MENÚ Y LÓGICA DEL JUEGO    ***********************************///
